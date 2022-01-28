3b777124100866e516306fe9935cacf8cb7834c3
samuaks <samusoisalo@hotmail.com>
2022-01-28 12:43:20 +0200

    initial commit

---
+++ components/light.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SingleLight = () => {
+
+    return (
+        <div className="light">
+        </div>
+    )
+}
+
+
+export default SingleLight;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
+
+const [lights, setLights] = useState([]);
+
+const socket = io('http://localhost:8000');
+
+
+useEffect(() => {
+  socket.on('lights', (data) => {
+    setLights(data.lights);
+  });
+}, []);
+
+
+const lightSwitch = (id) => {
+    socket.emit('switchId', {id: id});
+    setLights(lights.map(light => {
+      if (light.id === id) {
+        light.status = !light.status;
+      }
+      return light;
+    }));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>
+          Lights
+        </h1>
+        <div className="status-container">
+        {lights ? lights?.map((light, index) => {
+            return (
+                <div key={index} style={light.status ? {backgroundColor: 'green'} : {backgroundColor: 'red'}} className="light">
+                    <div className="light-status">
+                        <div className="light-status-switch" onClick={() => {
+                          lightSwitch(light.id);
+                        }}>
+                          {light.id}
+                        </div>
+                    </div>
+                </div>
+            )
+        }) : null}
+        </div>
+        </header>
     </div>
   );
 }
